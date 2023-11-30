cc4122e3e69c3eebd45fed48ab2130419e2c3b25
Frederick Morin <freddydumont@users.noreply.github.com>
2023-12-01 01:55:50 +0700

    Wrap projectile around screen

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -3,14 +3,13 @@ extends Area2D
 
 @export var damage: int = 2
 @export var speed: float = 1000
-@export var lifetime: float = 2
+@export var lifetime: float = 1
 
 var linear_velocity: Vector2 = Vector2.ZERO
 
-
-func shoot():
-	var direction := Vector2(cos(rotation), sin(rotation))
-	linear_velocity = direction * speed
+@onready var screen_size := get_viewport_rect().size
+## Adds half the sprite's (scaled) size to wrapping area so it's fully out of view before wrapping
+@onready var wrap_offset: float = ($Sprite2D.get_rect().size.x / 2) * $Sprite2D.scale.x
 
 
 func _ready():
@@ -21,6 +20,17 @@ func _ready():
 
 func _physics_process(delta: float):
 	position += linear_velocity * delta
+
+	# wraps the projectile around the screen
+	transform.origin = Vector2(
+		wrapf(transform.origin.x, -wrap_offset, screen_size.x + wrap_offset),
+		wrapf(transform.origin.y, -wrap_offset, screen_size.y + wrap_offset)
+	)
+
+
+func shoot():
+	var direction := Vector2(cos(rotation), sin(rotation))
+	linear_velocity = direction * speed
 
 
 ## triggers the hit() method when a collision is detected
