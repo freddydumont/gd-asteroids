46a679955ca63b320b689203ab7b94c80dc84c07
Frederick Morin <freddydumont@users.noreply.github.com>
2023-12-04 00:49:24 +0700

    Refactor UFO movement to prevent turnarounds

--- scripts/ufo.gd
+++ scripts/ufo.gd
@@ -7,10 +7,13 @@ enum SpawnSide { LEFT, RIGHT }
 @export var spawn_offset: float = 120
 
 @export_group("Movement")
-@export var speed: float = 120.0
-@export var change_time: float = 2.0
+@export var speed: float = 200.0
+@export var change_time: float = 0.250
+@export var max_angle_change: float = PI / 6
 
 var time_since_last_change: float = 0.0
+## Current direction as an angle
+var current_angle: float
 
 @onready var screen_size := get_viewport_rect().size
 
@@ -19,30 +22,32 @@ func _ready() -> void:
 	spawn_ufo()
 
 
+# TODO: shoot projectile at player at specified intervals
 func _process(delta: float) -> void:
-	# Randomize movement every `change_time` seconds
 	time_since_last_change += delta
+
+	# Slightly adjust the UFO's current angle randomly within the max_angle_change range
 	if time_since_last_change >= change_time:
 		time_since_last_change = 0.0
-		# TODO: correct movement so it doesn't turn around
-		velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * speed
+		current_angle += randf_range(-max_angle_change, max_angle_change)
+		velocity = Vector2(cos(current_angle), sin(current_angle)).normalized() * speed
 
 	move_and_collide(velocity * delta)
 
 
 func spawn_ufo() -> void:
-	var spawn_side := randi() % 2 as SpawnSide
 	var spawn_position := Vector2.ZERO
-	var direction_angle := randf_range(-PI / 4, PI / 4)
+	current_angle = randf_range(-PI / 4, PI / 4)
 
 	# Set spawn position and calculate initial velocity based on the side
-	match spawn_side:
+	match (randi() % 2) as SpawnSide:
 		SpawnSide.LEFT:
 			spawn_position.x = 0
-			velocity = Vector2(cos(direction_angle), sin(direction_angle)).normalized() * speed
 		SpawnSide.RIGHT:
 			spawn_position.x = screen_size.x
-			velocity = Vector2(-cos(direction_angle), sin(direction_angle)).normalized() * speed
+			current_angle = PI - current_angle
+
+	velocity = Vector2(cos(current_angle), sin(current_angle)).normalized() * speed
 
 	# random position along the y axis, with offset to prevent spawn in the corners
 	spawn_position.y = randf_range(spawn_offset, screen_size.y - spawn_offset)
